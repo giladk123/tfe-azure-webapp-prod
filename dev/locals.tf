c898fb4c44058a99f82d91c11fbba02054f556d5
giladk123 <giladk@gmail.com>
2023-12-15 08:02:08 +0200

    path

--- dev/locals.tf
+++ dev/locals.tf
@@ -1,8 +1,8 @@
 locals {
-  environment_path = "network.json"
+  environment_path = "./network.json"
   vnet_settings    = jsondecode(file(local.environment_path))
 
-  blob_path = "blob.json"
+  blob_path = "./blob.json"
   blob_settings = jsondecode(file(local.blob_path))
 
   subnet_objects = [
